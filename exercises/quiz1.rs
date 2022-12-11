bc897f05b56b1cf34230fbbcf1e996429a9e412e
Ivan Andre Scheel <ischeel@ad-cap.com.ar>
2022-12-11 11:39:54 -0300

    So far, so good

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,22 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(buy_amnt: i32) -> i32 {
 
+    let apple_cost: i32;
+
+    if buy_amnt > 40 {
+        apple_cost = 1
+    } else {
+        apple_cost = 2
+    }
+
+    return apple_cost * buy_amnt
+
+
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
