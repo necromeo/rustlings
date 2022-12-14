0cbc02ad1aac20854ab5fcb57f59c81de30d54b7
Ivan Andre Scheel <ischeel@ad-cap.com.ar>
2022-12-14 17:48:20 -0300

    HashMaps | Quiz incoming!

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -31,6 +30,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
+
         let team_1_name = v[0].to_string();
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
@@ -40,6 +40,25 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|e| e.goals_conceded += team_2_score)
+            .and_modify(|e| e.goals_scored += team_1_score)
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        scores.entry(team_2_name.clone())
+            .and_modify(|e| e.goals_conceded += team_1_score)
+            .and_modify(|e| e.goals_scored += team_2_score)
+            .or_insert(Team {
+            name: team_2_name,
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score.clone(),
+        });
     }
     scores
 }
